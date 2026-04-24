ba872e21ff32ba955287f06e2af468b5c1b39ea7
Leonardo <leonardozanidev@gmail.com>
2026-04-24 00:35:09 -0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mainAppContainer">
+        <header>
+          <h1>Job Board</h1>
+          <div className="searchContainer">
+            <input type="text" />
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
