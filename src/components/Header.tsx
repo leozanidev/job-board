0ba31752c8b13eec17fbe5797a34c28e02f94f71
Leonardo <leonardozanidev@gmail.com>
2026-04-26 13:43:02 -0300

    feat: adiciona o componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,28 +10,30 @@ interface props {
 const Header = ({ setSearchInput, setCategory, categoryList }: props) => {
   return (
     <header>
-      <div className="appTitle">
-        <h1>Job Board</h1>
-      </div>
-      <div className="searchSection">
-        <label>
-          <input
-            aria-label="Busca por termo "
-            type="text"
-            placeholder="Digite sua busca"
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-        </label>
-        <select
-          aria-label="Filtro por categoria"
-          name="category"
-          onChange={(e) => setCategory(e.target.value)}>
-          {categoryList.map((category) => (
-            <option key={category.id} value={category.slug}>
-              {category.name}
-            </option>
-          ))}
-        </select>
+      <div className="bg-sky-700 flex flex-col items-center justify-center w-1/1 p-10 gap-10">
+        <h1 className="text-7xl text-gray-300">Job Board</h1>
+        <div className="searchSection flex justify-center items-center gap-5 bg-sky-700">
+          <label>
+            <input
+              className="text-white border rounded-md p-2 h-10 focus:outline-none focus:ring-2 focus:ring-white"
+              aria-label="Busca por termo "
+              type="text"
+              placeholder="Digite sua busca"
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </label>
+          <select
+            className="text-gray-400 border rounded-md p-2 h-10 focus:outline-none focus:ring-2 focus:ring-white"
+            aria-label="Filtro por categoria"
+            name="category"
+            onChange={(e) => setCategory(e.target.value)}>
+            {categoryList.map((category) => (
+              <option key={category.id} value={category.slug}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </header>
   );
